e453f967d926c38d1faf8e6767a762b801e82900
adetokefad <adetokefaderin@gmail.com>
2025-03-09 00:30:21 +0100

    corrected error in index

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,22 +1,7 @@
-const year = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
+const currentYear = new Date().getFullYear();
 
-// Use the date object
-const today = new Date();
+document.getElementById("currentyear").textContent = currentYear;
 
-// Get the current year
-year.innerHTML = <span>${today.getFullYear()}</span>;
-
-// Get the last modified date
-const modifiedDate = new Date(document.lastModified);
-lastModified.innerHTML = `<span class="modified">Last Modification: ${new Intl.DateTimeFormat(
-  "en-US",
-  {
-    dateStyle: "short",
-    timeStyle: "short",
-  }
-).format(modifiedDate)}</span>`;
-
-if (!document.lastModified) {
-  lastModified.innerHTML = "Last Modification: Not Available";
-}
+document.getElementById(
+  "lastmodified"
+).textContent = `Last modified: ${document.lastModified}`;
