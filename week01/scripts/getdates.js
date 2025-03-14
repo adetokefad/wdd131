2e8d1fd406b084477ffee0c71bc65322bfaf7624
adetokefad <adetokefaderin@gmail.com>
2025-03-14 22:44:54 +0100

    corrected

--- week01/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-const currentYear = new Date().getFullYear();
-
-document.getElementById("currentyear").textContent = currentYear;
-
-document.getElementById(
-  "lastmodified"
-).textContent = `Last modified: ${document.lastModified}`;
